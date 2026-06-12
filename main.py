bbc9b396096c9879c815b053966bb5e1d5a4510d
Aline Rigo Ceolin <alinerigoceolin1998@gmail.com>
2026-06-12 18:24:14 -0300

    coleta de dados

---
+++ main.py
@@ -0,0 +1,56 @@
+import pandas
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+url = "https://www.saucedemo.com/"
+
+driver = webdriver.Firefox()
+driver.get(url)
+
+senha = "secret_sauce"
+username = "standard_user"
+
+elem_username = driver.find_element(By.ID, "user-name")
+elem_senha = driver.find_element(By.ID, "password")
+
+elem_senha.send_keys(senha)
+elem_username.send_keys(username)
+
+elem_login = driver.find_element(By.ID, "login-button")
+elem_login.click()
+
+dicionario = {"nome":[], "descricao":[], "preco":[]}
+
+descs = driver.find_elements(By.CLASS_NAME, "inventory_item_desc")
+precos = driver.find_elements(By.CLASS_NAME, "inventory_item_price")
+nomes = driver.find_elements(By.CLASS_NAME, "inventory_item_name")
+
+for d in descs:
+    dicionario["descricao"].append(d.text)
+
+for p in precos:
+    dicionario["preco"].append(p.text)
+
+for n in nomes:
+    dicionario["nome"].append(n.text)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+time.sleep(5)
+
